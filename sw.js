4a88f66e8533e0480b14080995992f2aa0962a00
Anderson <andersonimwsl@gmail.com>
2026-03-18 20:37:09 -0300

    fix manifest standalone theme black

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'hitssoud-v10';
+var CACHE = 'hitssoud-v11';
 var FILES = ['/', '/hitssoud.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', function(e) {
