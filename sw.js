90b1f20aa527cebc28c419dbffab9e69c32ad96a
Anderson <andersonimwsl@gmail.com>
2026-03-18 05:32:33 -0300

    update cache version v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'hitssoud-v1';
+var CACHE = 'hitssoud-v2';
 var ASSETS = ['/', '/hitssoud.html', '/manifest.json'];
 self.addEventListener('install', function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
